14b075f47dd8f6e96675960cb1723d3f914c8f8a
park-Jaturon <110178210+park-Jaturon@users.noreply.github.com>
2025-08-14 23:29:28 +0700

    update Controller Frontend
    TermController
    ProductDetailController
    NewsDetailController

--- app/Http/Controllers/NewsDetailController.php
+++ app/Http/Controllers/NewsDetailController.php
@@ -2,43 +2,51 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class NewsDetailController extends Controller
 {
     public function index($language, $id)
     {
-        $NewsData = collect([
-            [
-                'id' => 1,
-                'image' => '/image/Rectangle 169.png',
-                'name' => 'กิจกรรมวันเด็ก ปี 2567',
-                'date' => 'March 22, 2022',
-                'description' => 'บริษัทร่วมกิจกรรมสร้างความสุขในวันเด็กแจกของรางวัลมากมาย',
-            ],
-            [
-                'id' => 2,
-                'image' => '/image/ภาพ News-2.png',
-                'name' => 'กิจกรรมวันเด็ก ปี 2565',
-                'date' => 'March 27, 2021',
-                'description' => 'บริษัทร่วมกิจกรรมสร้างความสุขในวันเด็กแจกของรางวัลมากมาย',
-            ],
-            [
-                'id' => 3,
-                'image' => '/image/Home 1.jpg',
-                'name' => 'กิจกรรมวันเด็ก ปี 2564',
-                'date' => 'March 26, 2023',
-                'description' => 'บริษัทร่วมกิจกรรมสร้างความสุขในวันเด็กแจกของรางวัลมากมาย',
-            ],
-        ]);
+        // $NewsData = collect([
+        //     [
+        //         'id' => 1,
+        //         'image' => '/image/Rectangle 169.png',
+        //         'name' => 'กิจกรรมวันเด็ก ปี 2567',
+        //         'date' => 'March 22, 2022',
+        //         'description' => 'บริษัทร่วมกิจกรรมสร้างความสุขในวันเด็กแจกของรางวัลมากมาย',
+        //     ],
+        //     [
+        //         'id' => 2,
+        //         'image' => '/image/ภาพ News-2.png',
+        //         'name' => 'กิจกรรมวันเด็ก ปี 2565',
+        //         'date' => 'March 27, 2021',
+        //         'description' => 'บริษัทร่วมกิจกรรมสร้างความสุขในวันเด็กแจกของรางวัลมากมาย',
+        //     ],
+        //     [
+        //         'id' => 3,
+        //         'image' => '/image/Home 1.jpg',
+        //         'name' => 'กิจกรรมวันเด็ก ปี 2564',
+        //         'date' => 'March 26, 2023',
+        //         'description' => 'บริษัทร่วมกิจกรรมสร้างความสุขในวันเด็กแจกของรางวัลมากมาย',
+        //     ],
+        // ]);
 
-        $news = $NewsData->firstWhere('id', (int) $id);
+        $news = Post::where('id', (int) $id)
+        ->where('slug', 'NEWS')
+        ->where('language', $language)
+        ->first();
 
         if (!$news) {
-            abort(404, 'News not found');
+            // abort(404, 'News not found');
+             $news = Post::where('id', (int) $id)
+        ->where('slug', 'NEWS')
+        ->where('defaults', 1)
+        ->first();
         }
 
-        return view('pages.news-detail.news-detail', compact('news', 'language'));
+        return view('pages.news-detail.news-detail', compact('news'));
     }
 }
 
--- app/Http/Controllers/TermController.php
+++ app/Http/Controllers/TermController.php
@@ -2,12 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class TermController extends Controller
 {
-    public function index()
+    public function index($language)
     {
-        return view('pages.term.term');
+        $Term = Post::where('slug', 'TERM')
+            ->where('language', $language)->first();
+            
+        if (!$Term) {
+            $Term = Post::where('slug', 'TERM')
+                ->where('defaults', 1)->first();
+        }
+        return view('pages.term.term', compact('Term'));
     }
 }
