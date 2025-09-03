9a28337a3bba9c3ce99b3ee88256c664b216a7f9
TA GI <130833943+TaJessadaPhotiya@users.noreply.github.com>
2025-09-03 11:06:20 +0700

    update ta

--- resources/views/pages/home/section1.blade.php
+++ resources/views/pages/home/section1.blade.php
@@ -1,12 +1,12 @@
 <div class="relative w-full h-full">
     <div
-        class="2xl:w-[1300px] w-full min-h-screen 2xl:min-h-[calc(100vh-592px)] mx-auto md:px-[4rem] px-[1rem] xl:pt-[4rem] pt-[1rem] xl:pb-[6rem] pb-[4rem] max-md:shadow-2xl">
+        class="2xl:w-[1300px] w-full min-h-full 2xl:min-h-[calc(100vh-592px)] mx-auto md:px-[4rem] px-[1rem] xl:pt-[4rem] pt-[1rem] xl:pb-[6rem] pb-[4rem] 2xl:shadow-2xl">
         <div class="container mx-auto" data-aos="fade-up" data-aos-duration="1200">
             <h1 class="text-[#098C46] lg:text-2xl text-xl font-semibold text-center italic">
                 {{ $homeContent->title }}<br />
                 {{ $homeContent->description }}
             </h1>
-            <div >
+            <div>
               {!! $homeContent->content !!}
             </div>
         </div>
